08f26e830a085267422e02b463fa0dbe0925c2ba
Vitalii <tomrain78@gmail.com>
2024-11-10 23:32:57 +0200

    init app

---
+++ dist/bundle.js
@@ -0,0 +1,119 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scss/style.scss":
+/*!*****************************!*\
+  !*** ./src/scss/style.scss ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://dashboard/./src/scss/style.scss?");
+
+/***/ }),
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/style.scss */ \"./src/scss/style.scss\");\n/* harmony import */ var _modules_main__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/main */ \"./src/modules/main.js\");\n/* harmony import */ var _modules_main__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modules_main__WEBPACK_IMPORTED_MODULE_1__);\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://dashboard/./src/app.js?");
+
+/***/ }),
+
+/***/ "./src/modules/main.js":
+/*!*****************************!*\
+  !*** ./src/modules/main.js ***!
+  \*****************************/
+/***/ (() => {
+
+eval("const list = document.querySelector(\"[header-list]\");\r\nconst items = document.querySelectorAll(\"[header-list-item]\");\r\n\r\nconst state = {\r\n  currentIndex: 0,\r\n  scrollDirection: 1,\r\n};\r\n\r\nfunction scrollItems() {\r\n  let { currentIndex, scrollDirection } = state;\r\n  list.style.transition = \"transform 0.6s ease-in\";\r\n  list.style.transform = `translateY(-${currentIndex * 40}px)`;\r\n\r\n  currentIndex += scrollDirection;\r\n\r\n  if (currentIndex >= items.length) {\r\n    scrollDirection = -1;\r\n    currentIndex = items.length - 2;\r\n  } else if (currentIndex < 0) {\r\n    scrollDirection = 1;\r\n    currentIndex = 1;\r\n  }\r\n  state.currentIndex = currentIndex;\r\n  state.scrollDirection = scrollDirection;\r\n}\r\n\r\nfunction initApp() {\r\n  setInterval(scrollItems, 4000);\r\n}\r\n\r\ndocument.addEventListener(\"DOMContentLoaded\", () => {\r\n  initApp();\r\n});\r\n\n\n//# sourceURL=webpack://dashboard/./src/modules/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/app.js");
+/******/ 	
+/******/ })()
+;
---
+++ src/modules/main.js
@@ -0,0 +1,33 @@
+const list = document.querySelector("[header-list]");
+const items = document.querySelectorAll("[header-list-item]");
+
+const state = {
+  currentIndex: 0,
+  scrollDirection: 1,
+};
+
+function scrollItems() {
+  let { currentIndex, scrollDirection } = state;
+  list.style.transition = "transform 0.6s ease-in";
+  list.style.transform = `translateY(-${currentIndex * 40}px)`;
+
+  currentIndex += scrollDirection;
+
+  if (currentIndex >= items.length) {
+    scrollDirection = -1;
+    currentIndex = items.length - 2;
+  } else if (currentIndex < 0) {
+    scrollDirection = 1;
+    currentIndex = 1;
+  }
+  state.currentIndex = currentIndex;
+  state.scrollDirection = scrollDirection;
+}
+
+function initApp() {
+  setInterval(scrollItems, 4000);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initApp();
+});
